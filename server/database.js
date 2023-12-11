13a2ebef508d0dfbc33de29cfa17b0785b87b495
yassinoa <yessinedev6@gmail.com>
2023-12-12 00:38:44 +0100

    connect

--- server/database.js
+++ server/database.js
@@ -3,10 +3,7 @@ const mongoose =require("mongoose")
 
 const connectToDatabase= async()=>{
  try {
-      const connect =await mongoose.connect(process.env.MONGO_URL, {
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
-    })
+      const connect =await mongoose.connect(process.env.MONGO_URL)
     console.log(`MONGODB connected : ${connect.connection.host}`)
   } catch (error) {
     console.log(`Error : ${error.message}`)
